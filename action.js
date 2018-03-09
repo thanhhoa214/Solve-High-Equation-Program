920433246273c91e03205d6e576ff546610f98b7
thanhhoa214 <thanhthanhhoa123@gmail.com>
2018-03-09 15:57:40 +0700

    Solve high equation program

---
+++ action.js
@@ -0,0 +1,25 @@
+
+function hideValue() {
+   document.getElementById('data').value = '';
+}
+
+function showChecked() {
+   let regex = RegExp('^((-?\\d+[.]?(\\d+)?)(\\s+)?)+$', 'gm');
+   let input = document.getElementById('data').value;
+   if (!regex.test(input)) {
+      document.getElementById('status').innerHTML = 'Something\'s wrong. Please check again';
+      return false;
+   } else {
+      document.getElementById('status').innerHTML = 'It\'s good';
+      return true;
+   }
+}
+
+function showResult() {
+   if (showChecked()) {
+      let exp1 = new Expression(document.getElementById('data').value);
+      exp1.findRoot();
+      document.getElementById('show').innerHTML = exp1.printExp() + exp1.printRoot();
+   }
+}
+
